f869335aca743897708a0518415ced094f99c911
Your Name <your.email@example.com>
2025-01-30 17:28:37 -0500

    Task 1 - Product Price Management

---
+++ cc_3.js
@@ -0,0 +1,6 @@
+// Task 1: product prices
+let prices = [95,80,65,30,25]; // setting initial prices
+prices.push(99);
+prices.shift();
+console.log(prices) // logging updated prices after adding 99 and removing 95
+
