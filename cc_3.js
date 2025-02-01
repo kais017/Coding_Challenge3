c9e8f4ba25aece5dafff12b8912ac41459ab301e
Your Name <your.email@example.com>
2025-02-01 14:07:50 -0500

    Task 3 - Employee Performance Tracking

--- cc_3.js
+++ cc_3.js
@@ -11,3 +11,21 @@ let total = orders.reduce((sum, order) => sum + order, 0) // calculating the tot
 console.log("Orders:", orders)
 console.log("Total of orders:", total) 
 
+// Task 3: Employee Performance Tracking Scenario
+let employee = {
+    name: "Paul Atreides",
+    role: "Kwisatz Haderach",
+    performanceScore: 100,
+    isActive: true,
+    promotionEligible: true
+} // employee properties
+
+let UpdatedperformanceScore = 50; // updating the performance score
+let promotionEligible = UpdatedperformanceScore >= 80 ? "Promotion Eligible" : "Promotion Not Eligible"; 
+// applying the condition that the employee must have a performance score of 80 or higher to be eligible for a promotion
+
+console.log(employee); 
+console.log("Performance Score:", employee.performanceScore);
+console.log("Updated Performance Score:", UpdatedperformanceScore);
+console.log(promotionEligible);
+// Logging Task 3
